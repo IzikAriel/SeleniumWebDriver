e52a6511b1c9de495b7086e840141dbdfb3360fd
Itzik Ariel <izikariel95@gmail.com>
2021-01-26 12:57:43 +0200

    add Test hyperLink

---
+++ SeleniumWebDriver/ComponentHelper/GenericHelper.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumWebDriver.Setting;
+
+namespace SeleniumWebDriver.ComponentHelper
+{
+    public class GenericHelper
+    {
+
+        public static bool IsElementPresent(By locator)
+        {
+            try
+            {
+                bool res = ObjectRepository.Driver.FindElements(locator).Count == 1;
+                return res;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public static IWebElement GetElement(By locator)
+        {
+
+            if (IsElementPresent(locator) == true)
+            {
+                return ObjectRepository.Driver.FindElement(locator);
+            }
+            else throw new NoSuchElementException("Element Not Found: " + locator.ToString());
+
+        }
+
+
+
+
+    }
+}
---
+++ SeleniumWebDriver/TestScript/TextBox/TestTextBox.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebDriver.TestScript.TextBox
+{   
+    [TestClass]
+    public class TestTextBox
+    {
+
+        [TestMethod]
+
+
+
+    }
+}
