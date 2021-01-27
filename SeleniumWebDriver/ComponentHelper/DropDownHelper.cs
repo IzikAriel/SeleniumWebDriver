4d2484fcdc810b6e1d676a8b37a3a71a8f083b3d
Itzik Ariel <izikariel95@gmail.com>
2021-01-27 17:50:40 +0200

    Add Test DropDownList

---
+++ SeleniumWebDriver/ComponentHelper/DropDownHelper.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebDriver.ComponentHelper
+{
+   
+
+    public class DropDownHelper
+    {
+        private static IWebElement Element;
+
+        public static void SelectElementByIndex(By locator, int index)
+        {   
+           
+            Element = GenericHelper.GetElement(locator);
+            SelectElement select = new SelectElement(Element);
+            if (index < select.Options.Count)
+            {
+                select.SelectByIndex(index);
+            }
+            else
+            {
+                string ex = "There is a problem with a SelectElementByIndex function, index bigger than a count of the List";
+                Console.WriteLine(ex);
+                throw new Exception(ex);
+            }
+        }
+
+        public static void SelectElementByValue(By locator, string visbleElement)
+        {
+            Element = GenericHelper.GetElement(locator);
+            SelectElement select = new SelectElement(Element);
+            IList<string> allOptions = GetAllElements(locator);
+            
+            if (allOptions.Contains(visbleElement))
+            {
+                select.SelectByValue(visbleElement);
+            }
+            else
+            {
+                string ex = "There is a problem with a SelectElementByValue function, value not Found";
+                Console.WriteLine(ex);
+                throw new Exception(ex);
+            }
+          
+        }
+
+        private static IList<string> GetAllElements(By locator)
+        {
+            Element = GenericHelper.GetElement(locator);
+            SelectElement select = new SelectElement(Element);
+            return select.Options.Select((x) => x.Text).ToList();
+        }
+
+        public static void PrintAllElements(By locator)
+        {
+            IList<string> texts = GetAllElements(locator);
+            foreach(string text in texts)
+            {
+                Console.WriteLine(text);
+            }
+        }
+
+
+    }
+}
---
+++ SeleniumWebDriver/TestScript/HandleDropDown/DropDownList.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumWebDriver.ComponentHelper;
+using SeleniumWebDriver.Setting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebDriver.TestScript.HandleDropDown
+{   
+    [TestClass]
+    public class DropDownList
+    {
+        [TestMethod]
+        public void DropDown()
+        {
+            string url = ObjectRepository.Config.GetWebsite();
+            NavigationHelper.NavigateToUrl(url);
+            LinkHelper.ClickLink(By.LinkText("File a Bug"));
+
+            By userTextBox = By.Id("Bugzilla_login");
+            string user = ObjectRepository.Config.GetUserName();
+            TextBoxHelper.TypeinTextBox(userTextBox, user);
+
+            By passwordTextBox = By.Id("Bugzilla_password");
+            string password = ObjectRepository.Config.GetPassword();
+            TextBoxHelper.TypeinTextBox(passwordTextBox, password);
+
+            By loggingButton = By.Id("log_in");
+            ButtonHelper.ClickButton(loggingButton);
+
+            By listLoactor = By.Id("bug_severity");
+            DropDownHelper.SelectElementByIndex(listLoactor, 2);
+            DropDownHelper.SelectElementByValue(listLoactor, "blocker");
+            DropDownHelper.PrintAllElements(listLoactor);
+        }
+    }
+}
